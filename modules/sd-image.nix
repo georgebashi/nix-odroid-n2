bb416828aea3639e156f6d4c5cb5431beab26958
Delta <d4delta@outlook.fr>
2021-10-06 14:12:53 +0200

    switch from cd-dvd to sd-image, because it's deprecated

--- modules/sd-image.nix
+++ modules/sd-image.nix
@@ -2,7 +2,7 @@
 
 {
   imports = [
-    <nixpkgs/nixos/modules/installer/cd-dvd/sd-image-aarch64.nix>
+    <nixpkgs/nixos/modules/installer/sd-card/sd-image-aarch64-installer.nix>
     ./profile.nix
   ];
 
